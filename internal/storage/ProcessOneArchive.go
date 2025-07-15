dc206340b9f8d708fcd37b7ef0974bddeb6806f5
delorency <delorency>
2025-07-16 02:26:51 +0300

    add README.md

--- internal/storage/ProcessOneArchive.go
+++ internal/storage/ProcessOneArchive.go
@@ -7,20 +7,16 @@ import (
 )
 
 func (s *storage) ProcessOneArchive(arch *archive) {
-
 	arch.mu.Lock()
+	defer arch.mu.Unlock()
 
 	arch.Status = StatusInProgress
-
-	arch.mu.Unlock()
 
 	files := make(map[string][]byte)
 	for _, url := range arch.Files {
 		data, err := internal.DownloadFile(url)
 		if err != nil {
-			arch.mu.Lock()
 			arch.Errors = append(arch.Errors, fmt.Sprintf("Ошибка загрузки %s", url))
-			arch.mu.Unlock()
 
 			continue
 		}
@@ -29,18 +25,15 @@ func (s *storage) ProcessOneArchive(arch *archive) {
 	}
 
 	zipBuffer, err := internal.CreateZipArchive(files)
-	arch.mu.Lock()
 	if err != nil {
 		arch.Errors = append(arch.Errors, "Ошибка создания архива")
 		arch.Status = StatusFailed
 
-		arch.mu.Unlock()
 	} else {
 		arch.ZipBuffer = zipBuffer
 		arch.ZipName = arch.ID + ".zip"
 		arch.Status = StatusCompleted
 
-		arch.mu.Unlock()
 		s.CompleteArchive(arch.ID)
 	}
 }
